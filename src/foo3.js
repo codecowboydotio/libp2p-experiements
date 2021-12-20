49eec3b2c7658d2c6cdac9bb4c55ad34eb660af9
codecowboydotio <scott@vankalken.org>
2021-12-20 16:29:05 +1100

    updated config

--- src/foo3.js
+++ src/foo3.js
@@ -66,7 +66,6 @@ const request = require('request')
     request(conf_url, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       console.log(body)
-      console.log(res)
     })  
   })
 
